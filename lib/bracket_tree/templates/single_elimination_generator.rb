e685eb6f36a7b66af18ec16ed849a511054746fb
andrea longhi <andrea@spaghetticode.it>
2014-07-01 13:32:51 +0200

    add #populate_first_row_matches
    set starting_seats
    
    build matches with basic hash already populated

--- lib/bracket_tree/templates/single_elimination_generator.rb
+++ lib/bracket_tree/templates/single_elimination_generator.rb
@@ -1,11 +1,14 @@
+require 'active_support/all'
+
 module BracketTree
   module Template
     class SingleEliminationGenerator
       attr_reader :seats, :matches, :starting_seats, :contenders
 
       def initialize(contenders)
-        @contenders = contenders
-        @matches    = []
+        @contenders     = contenders
+        @matches        = []
+        @starting_seats = (1..contenders*2).select { |n| n.odd? }
       end
 
       def matches_for_row(n)
@@ -20,7 +23,7 @@ module BracketTree
         i = 1
         while matches_for_row(i) > 0
           row = []
-          matches_for_row(i).times  { row << {} }
+          matches_for_row(i).times  { row << {:seats => nil, :winner_to => nil, :loser_to => nil} }
           matches << row
           i += 1
         end
@@ -28,6 +31,18 @@ module BracketTree
 
       def matches_row(n)
         matches[n]
+      end
+
+      def populate_matches
+        populate_first_row_matches
+      end
+
+      def populate_first_row_matches
+        starting_seats.in_groups_of(2).each_with_index do |arr, i|
+          step ||= (arr[1] - arr[0])/2
+          matches_row(0)[i][:seats] = arr
+          matches_row(0)[i][:winner_to] = arr[1] - step
+        end
       end
     end
   end
--- spec/single_elimination_json_generator_spec.rb
+++ spec/single_elimination_json_generator_spec.rb
@@ -13,6 +13,10 @@ module BracketTree
           subject.matches.flatten.size.should == 15
         end
 
+        it 'has 16 starting seats' do
+          subject.starting_seats.size.should == 16
+        end
+
         it 'has 8 matches in the first row' do
           subject.matches_row(0).size.should == 8
         end
@@ -23,6 +27,14 @@ module BracketTree
             subject.matches_for_row(2).should == 4
             subject.matches_for_row(3).should == 2
             subject.matches_for_row(4).should == 1
+          end
+        end
+
+        describe '#populate_first_row_matches' do
+          it 'builds seats' do
+            subject.populate_first_row_matches
+            subject.matches_row(0).first.should == {:seats => [1,3], :winner_to => 2, :loser_to => nil}
+            subject.matches_row(0).last.should == {:seats => [29,31], :winner_to => 30, :loser_to => nil}
           end
         end
       end
